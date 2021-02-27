c855392ad9f524816a7d2b1589a82dcab10319d1
BIKRAM <69245868+dbikram@users.noreply.github.com>
2021-02-27 18:47:12 +0530

    Delete Maximum_number.py

--- Maximum_number.py
+++
@@ -1,8 +0,0 @@
-# Maximum of two numbers
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-if a>b:
-    m=a
-else:
-    m=b
-print("The maximum number of {0} and {1} is {2}".format(a,b,m))
