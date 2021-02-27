305ce2c971df9a5dd81d45c29d05d673f3e69429
BIKRAM <69245868+dbikram@users.noreply.github.com>
2021-02-27 18:47:23 +0530

    Delete Calculation.py

--- Calculation.py
+++
@@ -1,7 +0,0 @@
-# python program to add two numbers
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-print("The sum of {0} and {1} is {2}".format(a,b,(a+b)))
-print("The multiplication of {0} and {1} is {2}".format(a,b,a*b))
-print("The division of {0} and {1} is {2}".format(a,b,(a/b)))
-print("The subtraction of {0} and {1} is {2}".format(a,b,(a-b)))
